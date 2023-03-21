715006610888a6c515eaa0dc6b6d19a9ea653d45
abdelilah <labdlilah68@gmail.com>
2023-03-21 16:58:24 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/* A program that prints _putchar
+ *  followed by a new line.
+*/
+
+int main(void){
+	_putchar(A);
+	_putchar(B);
+	_putchar(D);
+	_putchar(E);
+	_putchar(L);
+	_putchar(I);
+	_putchar(L);
+	_putchar(A);
+	_putchar(H);
+	_putchar(\n:);
+return(0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* A function that print alphatbets
+ * in lowercase
+ * in upercase
+*/
+
+void print_alphabet(void){
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+
+	
+}
