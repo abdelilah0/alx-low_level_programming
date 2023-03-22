35d27ac20ac8441dceb283a674b1b18b57a676b9
abdelilah <labdlilah68@gmail.com>
2023-03-21 17:10:04 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
+/**
+ * main - print _putchar
+ * is written by abdelilah
+ * Return: 0
+ */
 
-/* A program that prints _putchar
- *  followed by a new line.
-*/
-
-int main(void){
-	_putchar(_);
-	_putchar(p);
-	_putchar(u);
-	_putchar(t);
-	_putchar(c);
-	_putchar(h);
-	_putchar(a);
-	_putchar(r);
-	_putchar(\n:);
-return(0);
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
