fa09c42093ab55463a4a4461926d5764327fc85c
abdelilah <labdlilah68@gmail.com>
2023-04-06 15:54:23 -0700

    0x08-recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,14 +7,14 @@
  */
 int _strlen_recursion(char *s)
 {
-	int  = 0;
+	int longit = 0;
 
 	if (*s)
 	{
-		l++;
-		l += _strlen_recursion(s + 1);
+		longit++;
+		longit += _strlen_recursion(s + 1);
 	}
 
-	return (l);
+	return (longit);
 }
 
