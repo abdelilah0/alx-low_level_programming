5ac19897ec085737aea9b5d86594333a8d11964d
abdelilah <labdlilah68@gmail.com>
2023-04-21 17:37:33 -0700

    variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,16 +15,15 @@
 		va_list ap;
 		unsigned int i, sum = 0;
 
+		if(n != 0){
+		va_start(ap, m);
 
-		va_start(ap, n);
-
-
-		for (i = 0; i < n; i++)
+		for (i = 0; i < m; i++)
 			sum += va_arg(ap, int);
 
 
 		va_end(ap);
-
+		}else{ return 0;}
 
 		return (sum);
 	}
